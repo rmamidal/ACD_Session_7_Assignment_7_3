92ccff153e0014e56b14d152004c802c33730192
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-05-03 10:20:06 +0530

    Added logic

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 function counter() {
-	var cntr =1;
+	var cntr = 1;
 
 	function incrementcntr() {
-	  cntr ++;
+	  return cntr++;
 	}
 
-	return cntr;
+	return incrementcntr;
 }
+var increment = counter();
 
-console.log(counter());
-console.log(counter());
-console.log(counter());
+console.log(increment());
+console.log(increment());
+console.log(increment());
+
+var increment2 = counter();
+
+console.log(increment2());
+console.log(increment2());
+console.log(increment2());
+console.log(increment2());
