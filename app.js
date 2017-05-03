80a6e1e63509b5aad610d76e082092b210771f94
Rajendhar Mamidala <rajendhar.mamidala@capgemini.com>
2017-05-03 12:46:37 +0530

    Added code comments

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
+// Function with closure to increment counter.
 function counter() {
 	var cntr = 1;
 
+  // Closure to increment outer fucntion counter.
 	function incrementcntr() {
-	  return cntr++;
+	  return cntr++; // Post increament counter.
 	}
 
-	return incrementcntr;
+	return incrementcntr; // Return closure function.
 }
-var increment = counter();
 
-console.log(increment());
-console.log(increment());
-console.log(increment());
+var increment = counter(); // Calling Counter function (outer) to set the counter = 1;
 
-var increment2 = counter();
+console.log(increment()); // Calling closure function to increment counter = 1.
+console.log(increment()); // Calling closure function to increment counter = 2.
+console.log(increment()); // Calling closure function to increment counter = 3.
 
-console.log(increment2());
-console.log(increment2());
-console.log(increment2());
-console.log(increment2());
+var increment2 = counter(); // Calling Counter function (outer) to set the counter = 1;
+
+console.log(increment2()); // Calling closure function to increment counter = 1.
+console.log(increment2()); // Calling closure function to increment counter = 2.
+console.log(increment2()); // Calling closure function to increment counter = 3.
+console.log(increment2()); // Calling closure function to increment counter = 4.
